1636ec5b18c75dda21397032aca638365dd91add
renyujie518 <903356741@qq.com>
2022-06-13 17:09:52 +0800

    03-update

--- src/main/java/com/example/geekbang/spring/class3/example2/BeanConfig.java
+++ src/main/java/com/example/geekbang/spring/class3/example2/BeanConfig.java
@@ -9,6 +9,7 @@ import java.util.List;
 /**
  * @program: geekbang_springDemo
  * @description: 案例 2：错乱的注入集合
+ * 需求： 存在多个学生 Bean，我们需要找出来，并存储到一个 List 里面去
  * @author: gao wei
  * @create: 2022-01-25 15:35
  */
@@ -28,6 +29,11 @@ public class BeanConfig {
     //    return createStudent(2, "fang");
     //}
 
+
+    /*
+     *方式二:直接装配
+     *方式二不能与方式一共存
+     */
     //@Bean
     //public List<Student> students() {
     //    Student student3 = createStudent(3, "liu");
@@ -35,11 +41,10 @@ public class BeanConfig {
     //    return Arrays.asList(student3, student4);
     //}
 
-    /*
-     *方式二:直接装配
-     * 方式二不能与方式一共存
-     */
 
+    /**
+     * @description 使用直接装配的方式保证收集到所有的Student
+     */
     @Bean
     public List<Student> students() {
         Student student1 = createStudent(1, "xie");
@@ -48,6 +53,28 @@ public class BeanConfig {
         Student student4 = createStudent(4, "fu");
         return Arrays.asList(student1, student2, student3, student4);
     }
+
+
+    /**
+     * @description 使用收集的方式保证收集到所有的Student
+     */
+    //@Bean
+    //public Student student1(){
+    //    return createStudent(1, "xie");
+    //}
+    //@Bean
+    //public Student student2(){
+    //    return createStudent(2, "fang");
+    //}
+    //@Bean
+    //public Student student3(){
+    //    return createStudent(3, "liu");
+    //}
+    //@Bean
+    //public Student student4(){
+    //    return createStudent(4, "fu");
+    //}
+
 
     private Student createStudent(int id, String name) {
         Student student = new Student();
--- src/main/java/com/example/geekbang/spring/class3/example2/C3E2StudentController.java
+++ src/main/java/com/example/geekbang/spring/class3/example2/C3E2StudentController.java
@@ -18,8 +18,10 @@ import java.util.List;
 @Slf4j
 public class C3E2StudentController {
 
+    //备注 这里在注入的时候不需要加@Autowired
     private List<Student> students;
 
+    //显示声明构造器的时候，bean创建会以此创建
     public C3E2StudentController(List<Student> students){
         this.students = students;
     }
